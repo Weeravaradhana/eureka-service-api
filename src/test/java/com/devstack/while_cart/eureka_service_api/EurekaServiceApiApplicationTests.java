e6728e79c5d897fa31d6451cb5681aded698174a
Weeravaradhana <thaminduweeravardana@gmail.com>
2025-07-19 15:06:53 +0530

    Initial commit

---
+++ src/test/java/com/devstack/while_cart/eureka_service_api/EurekaServiceApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devstack.while_cart.eureka_service_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServiceApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
